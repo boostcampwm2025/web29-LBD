f5179406dc3ea2901865a1088a0b20e645f2bb69
vvzvvv <tmdgusv@gmail.com>
2026-01-06 18:13:25 +0900

    refactor: ProblemsController를 서비스 레이어와 연결 - ProblemsService를 DI로 주입 - 하드코딩된 응답 제거하고 서비스 메서드 호출

--- apps/server/src/problems/problems.controller.ts
+++ apps/server/src/problems/problems.controller.ts
@@ -1,18 +1,16 @@
 import { Controller, Post, Param, Body } from '@nestjs/common';
 import { SubmitRequestDto } from './dto/submit-request.dto';
+import { ProblemsService } from './problems.service';
 
 @Controller('problems')
 export class ProblemsController {
+  constructor(private readonly problemsService: ProblemsService) {}
+
   @Post(':problemId/submit')
   submit(
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     @Param('problemId') problemId: string,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     @Body() body: SubmitRequestDto,
   ) {
-    return {
-      result: 'PASS',
-      feedback: [],
-    };
+    return this.problemsService.submit(problemId, body);
   }
 }
